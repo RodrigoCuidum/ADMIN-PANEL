e7c6a94b34059d17225b4ec5cb5cc1cb3eca43a5
Rodrigo Mendez <rodrigo.mendez@cuidum.com>
2024-11-12 15:43:53 +0100

    COMPLETED sidebar

---
+++ panel-admin/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,85 @@
+import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import GroupRoundedIcon from '@mui/icons-material/GroupRounded';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+import CreditCardIcon from '@mui/icons-material/CreditCard';import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';import QuestionAnswerRoundedIcon from '@mui/icons-material/QuestionAnswerRounded';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top">
+            <span className="logo">lamadmin</span>
+        </div>
+        <hr />
+        <div className="center">
+            <ul>
+                <p className="title">MAIN</p>
+                <li>
+                    <DashboardIcon className="icon" />
+                    <span>Dashboard</span>
+                </li>
+                <p className="title">LISTS</p>
+                <li>
+                    <GroupRoundedIcon className="icon" />
+                    <span>Users</span>
+                </li>
+                <li>
+                    <StorefrontOutlinedIcon className="icon" />
+                    <span>Products</span>
+                </li>
+                <li>
+                    <CreditCardIcon className="icon" />
+                    <span>Orders</span>
+                </li>
+                <li>
+                    <LocalShippingIcon className="icon" />
+                    <span>Delivery</span>
+                </li>
+                <p className="title">USEFUL</p>
+                <li>
+                    <InsertChartIcon className="icon" />
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <QuestionAnswerRoundedIcon className="icon" />
+                    <span>Notifications</span>
+                </li>
+                <p className="title">SERVICE</p>
+                <li>
+                    <SettingsSystemDaydreamIcon className="icon" />
+                    <span>System Health</span>
+                </li>
+                <li>
+                    <PsychologyOutlinedIcon className="icon" />
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <SettingsRoundedIcon className="icon" />
+                    <span>Settings</span>
+                </li>
+                <p className="title">USER</p>
+                <li>
+                    <AccountCircleOutlinedIcon className="icon" />
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <LogoutRoundedIcon className="icon" />
+                    <span>Logout</span>
+                </li>
+            </ul>
+        </div>
+        <div className="bottom">
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
