d21a93004dd684a10614a4bf6651455ce9700f9c
Rodrigo Mendez <rodrigo.mendez@cuidum.com>
2024-11-15 16:47:33 +0100

    MODIFIED

--- panel-admin/src/components/chart/Chart.jsx
+++ panel-admin/src/components/chart/Chart.jsx
@@ -1,9 +1,63 @@
-import "./chart.scss"
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Jan", Total: 2100 },
+  { name: "Feb", Total: 1500 },
+  { name: "Mar", Total: 1800 },
+  { name: "Apr", Total: 1600 },
+  { name: "May", Total: 2600 },
+  { name: "Jun", Total: 2100 },
+  { name: "Jul", Total: 2300 },
+  { name: "Ago", Total: 2700 },
+  { name: "Sep", Total: 2200 },
+  { name: "Oct", Total: 2900 },
+  { name: "Nov", Total: 3300 },
+  { name: "Dec", Total: 2900 },
+];
 
 const Chart = () => {
   return (
-    <div className="chart">Chart</div>
-  )
-}
+    <div className="chart">
+      <div className="title">Last Year</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorTotal" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="50%" stopColor="rgba(208, 23, 179, 0.8)" stopOpacity={0.9} />
+              <stop offset="95%" stopColor="rgba(94, 236, 222, 0.8)" stopOpacity={0.8} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <XAxis dataKey="name" stroke="gray" />
+          <YAxis stroke="gray" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="rgba(208, 23, 179, 0.8)"
+            fill="url(#colorTotal)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
-export default Chart
+export default Chart;
--- panel-admin/src/components/featured/Featured.jsx
+++ panel-admin/src/components/featured/Featured.jsx
@@ -2,6 +2,8 @@ import "./featured.scss"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
 import { CircularProgressbar } from "react-circular-progressbar"
 import "react-circular-progressbar/dist/styles.css"
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined"
 
 const Featured = () => {
   return (
@@ -15,10 +17,33 @@ const Featured = () => {
                 <CircularProgressbar value={31} text={"31%"} strokeWidth={4} />
             </div>
             <p className="title">Total sales made today</p>
-            <p className="amount">$420</p>
+            <p className="amount">420€</p>
             <p className="desc">
                 Previous transactions processing. Last payments may not be included.
             </p>
+            <div className="summary">
+                <div className="item">
+                    <div className="itemTitle">Last Week</div>
+                    <div className="itemResult negative">
+                    <KeyboardArrowDownIcon fontSize="small" />
+                        <div className="resultAmount">1.2k €</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Month</div>
+                    <div className="itemResult positive">
+                    <KeyboardArrowUpOutlinedIcon fontSize="small" />
+                        <div className="resultAmount">2.4k €</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Year</div>
+                    <div className="itemResult positive">
+                    <KeyboardArrowUpOutlinedIcon fontSize="small" />
+                        <div className="resultAmount">12.4k €</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
